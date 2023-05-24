59d5828647602104846bd08a5a6ba647c95756b6
Dedokrmntha <dedokarmanata12@gmail.com>
2023-05-24 20:54:13 +0800

    first commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+class Home extends CI_Controller
+{
+    
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('Auth');
+		$this->Auth->cek_login();
+  }
+
+  public function index()
+  {
+    $this->load->view('vw_user/header');
+    $this->load->view('vw_user/navbar');
+    $this->load->view('vw_user/body');
+    $this->load->view('vw_user/footer');
+  }
+}
+   
+
+
+/* End of file Home.php.php */
+/* Location: ./application/controllers/Home.php.php */
---
+++ application/views/admin/login.php
@@ -0,0 +1,60 @@
+<div class="container-fluid bg-registration py-5" style="margin: 90px 0;">
+        <div class="container py-5">
+            <div class="row align-items-center">
+                <div class="col-lg-7 mb-5 mb-lg-0">
+                    <div class="mb-4">
+                        <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">HALAMAN</h6>
+                        <h1 class="text-white"><span class="text-primary">LOGIN#</span>ADMIN</h1>
+                    </div>
+                    <p class="text-white"></p>
+                    <ul class="list-inline text-white m-0">
+                        <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Masukkan Username Anda!</li>
+                        <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Masukkan Password Anda!</li>
+                        <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Klik Login Untuk Masuk Kehalaman Website!</li>
+                        <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Klik Register Jika Belum Mempunyai Akun!</li>
+                    </ul>
+                </div>
+                <div class="col-lg-5">
+                    <div class="card border-0">
+                        <div class="card-header bg-primary text-center p-4">
+                            <h1 class="text-white m-0">Login</h1>
+                        </div>
+                        <div class="card-body rounded-bottom bg-white p-5">
+                        <?php 
+				    if($this->session->flashdata('error') !='')
+				{
+					echo '<div class="alert alert-danger" role="alert">';
+					echo $this->session->flashdata('error');
+					echo '</div>';
+				}
+				?>
+ 
+				<?php 
+				if($this->session->flashdata('success_register') !='')
+				{
+					echo '<div class="alert alert-info" role="alert">';
+					echo $this->session->flashdata('success_register');
+					echo '</div>';
+				}
+				?>
+                    <form method="post" action="<?php echo base_url(); ?>Login/proses">
+					<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" class="form-control" name="username" id="username" placeholder="Enter Username">
+					</div>
+					<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" name="password" id="password" placeholder="Password">
+					</div>
+					<button type="submit" class="btn btn-primary">Login</button>
+				        </form>
+                        </div>
+                        <div class="text-center">
+								Belum Punya Akun? <a href="<?= base_url('Register/index') ?>" class="text-dark">Buat Akun</a>
+							</div>
+                        </body>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
